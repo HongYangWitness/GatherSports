4968c1572945c37d25b349f502b3a2ac3734881f
Yunchun <yup086@ucsd.edu>
2021-12-08 11:22:09 -0600

    updated controller files

--- backend/sportgather/src/main/java/com/example/sportgather/controller/OverViewController.java
+++ backend/sportgather/src/main/java/com/example/sportgather/controller/OverViewController.java
@@ -1,40 +1,70 @@
 package com.example.sportgather.controller;
 
-import com.example.sportgather.domain.Reservation;
-import com.example.sportgather.domain.User;
-import com.example.sportgather.service.OverViewService;
+import com.example.sportgather.domain.*;
+import com.example.sportgather.service.AppointmentService;
+import com.example.sportgather.service.EnrollmentService;
 import com.example.sportgather.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = "overview")
+@CrossOrigin(origins = "http://localhost:3000")
 public class OverViewController {
 
-    private final OverViewService overViewService;
     private final ReservationService reservationService;
+    private final EnrollmentService enrollmentService;
+    private final AppointmentService appointmentService;
 
     @Autowired
-    public OverViewController(OverViewService overViewService, ReservationService reservationService) {
-        this.overViewService = overViewService;
+    public OverViewController(ReservationService reservationService, EnrollmentService enrollmentService, AppointmentService appointmentService) {
         this.reservationService = reservationService;
+        this.enrollmentService = enrollmentService;
+        this.appointmentService = appointmentService;
     }
 
-    @GetMapping(path = "/{id}")
-    public List<Reservation> findReservationById(@PathVariable("id") String UserId){
-        System.out.println("findReservationById is called");
-        return reservationService.queryReservationByUserId(UserId);
+    // reservation operations
+    @GetMapping("/reservation")
+    public List<Reservation> findReservationById(@RequestParam("id") String userId){
+        List<Reservation> list = reservationService.findAllReservationByUserId(userId);
+        for (Reservation reservation: list){
+            System.out.println(reservation.getReservationId());
+        }
+        return list;
     }
 
-    @GetMapping(path = "/sportstar")
-    public List<Map.Entry<String, Integer>> findSportStar(){
+    @GetMapping(path = "/reservationstar")
+    public List<ReservationStar> findSportStar(){
         System.out.println("findSportStar is called");
-        return reservationService.querySportStar();
+        return reservationService.queryReservationStar();
+    }
+
+    @CrossOrigin
+    @PostMapping(path = "/reservation/cancel/{reservationid_delete}")
+    public void cancelReservation(@PathVariable("reservationid_delete") String reservationId){
+        System.out.println("Receiving request for deleting " + reservationId);
+        reservationService.deleteReservation(reservationId);
+    }
+
+    @GetMapping("/enrollment")
+    public List<Course> findEnrollmentById(@RequestParam("id") String userId){
+        System.out.println("Receiving request for deleting " + userId);
+        return enrollmentService.findEnrollmentByUser(userId);
+    }
+
+    //waiting change to post method
+    @CrossOrigin
+    @GetMapping("/enrollment/cancel/{userId}/{courseId}")
+    public void cancelEnrollment(@PathVariable("userId") String userId, @PathVariable("courseId")String courseId){
+        enrollmentService.deleteEnrollment(userId, courseId);
+    }
+
+    @GetMapping("/appointment/{status}")
+    public List<AppointmentInfo> findAcceptedAppointment(@PathVariable("status") String status, @RequestParam("id") String userId){
+        System.out.println(status);
+        return appointmentService.findAcceptAppointment(userId, status);
     }
 }
